127b034f4416b85a5f093c9d52f785b45c0ffab8
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-03-10 17:35:37 +0100

    Added Button component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
+import Button from "./Button";
 
 interface BannerProps {}
 
@@ -15,9 +17,12 @@ const Banner: FC<BannerProps> = ({}) => {
           <span className='block'>Driven By Values</span>
         </h1>
         <div>
-          {/* <Button title='Buy Now'></Button> */}
+          <Button title='Buy Now'></Button>
           <Link href='/'></Link>
         </div>
+      </div>
+      <div className='relative hidden duration-500 transition-all md:inline lg:h-[650px] lg:w-[600px]'>
+        <Image src='/iphone.png' height={600} width={600} alt='banner Image' />
       </div>
     </section>
   );
