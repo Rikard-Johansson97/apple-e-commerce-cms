9da77b482758e31b7acaa3be5bf5f78ab3ee7fdf
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-03-13 10:16:02 +0100

    added meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta
+          name='google-site-verification'
+          content='aQVE-oncwDAlrOnesKQwYHXfGrwMvTGVBQycEgb0JEU'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
